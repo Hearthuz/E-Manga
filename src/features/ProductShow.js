7c16cebf8e3c7b61722e62b70d162357d4331b20
SoStellar <githubpkaddress@gmail.com>
2022-09-22 19:09:46 +0700

    Complete productshow but no responsive

--- src/features/ProductShow.js
+++ src/features/ProductShow.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { FiHeart } from "react-icons/fi";
+import { BsStar } from "react-icons/bs";
 
 
 function ProductShow({ className }) {
@@ -29,16 +30,48 @@ function ProductShow({ className }) {
                         <div class="col-3 frontTag p-4 pr-6">BookType</div>
                         <div class="col-3 backTag p-4 pr-6">BookType</div>
                     </div>
-                    <div class="row d-flex justify-content-center p-2">
-                        <div class="col-3 tryBtn" role="button">Try it!</div>
-                        <div class="col-3 buyBtn" role="button">Buy it!</div>
-                        <div class="col-3 addFavBtn" role="button"><FiHeart /></div>
+                    <div class="row d-flex justify-content-around">
+                        <button class="col-3 tryBtn btn btn-outline-light shadow-sm" role="button">Try it!</button>
+                        <button class="col-3 buyBtn btn btn-outline-success" role="button">Buy it!</button>
+                        <button class="col-3 addFavBtn btn btn-outline-danger" role="button"><FiHeart /></button>
+                    </div>
+                    <div class="row p-4">
+                        <div class="d-flex justify-content-around align-items-center">
+                            <div class="ratings">
+                                <i><BsStar /></i>
+                                <i><BsStar /></i>
+                                <i><BsStar /></i>
+                                <i><BsStar /></i>
+                                <i><BsStar /></i>
+                            </div>
+                            <h5 class="review-count">12 Reviews</h5>
+                        </div>
+                    </div>
+                    <div class="row py-2 px-4">
+                        <span class="col-3 seriesName border-bottom border-black">Series :</span>
+                        <span class="col-9 seriesName d-flex justify-content-end border-bottom border-black">Lorem ipsum dolor sit amet</span>
+                    </div>
+                    <div class="row py-2 px-4">
+                        <span class="col-sm-3 fileType border-bottom border-black">File type :</span>
+                        <span class="col-sm-9 fileType d-flex justify-content-end border-bottom border-black">PDF</span>
+                    </div>
+                    <div class="row py-2 px-4">
+                        <span class="col-sm-3 sellDate border-bottom border-black">Release date :</span>
+                        <span class="col-sm-9 sellDate d-flex justify-content-end border-bottom border-black">dd/mm/yyyy</span>
+                    </div>
+                    <div class="row py-2 px-4">
+                        <span class="col-sm-3 bookLength border-bottom border-black">Length :</span>
+                        <span class="col-sm-9 bookLength d-flex justify-content-end border-bottom border-black">xxx pages</span>
+                    </div>
+                    <div class="row py-2 px-4">
+                        <span class="col-sm-3 price border-bottom border-black">Price :</span>
+                        <span class="col-sm-9 price d-flex justify-content-end border-bottom border-black">xxx ฿</span>
                     </div>
                 </div>
             </div>
             <div class="row p-4">
-                <div class="row bg-white synopsisBlock">
-                    <span>Synopsis</span>
+                <div class="row bg-white synopsisBlock d-flex justify-content-center align-items-center fit-content ">
+                    <span class="synopsisText">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean massa tortor, vestibulum quis semper sit amet, consectetur sed ante. Pellentesque at libero hendrerit sapien porttitor molestie. Praesent congue ante non orci auctor vulputate. Proin ut leo vitae lectus tristique sodales. Cras at euismod elit. Pellentesque diam risus, posuere non nunc at, commodo feugiat lectus. Aenean sollicitudin velit vitae mauris fermentum tempus. Praesent gravida purus eu cursus condimentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam auctor, nibh a pharetra semper, justo lacus vestibulum enim, sit amet mattis felis odio ultricies nisl. Etiam at quam hendrerit, blandit est a, aliquet ex. Fusce ac sem turpis. Sed porta, risus sed congue auctor, quam turpis fermentum mi, ut tristique sapien purus in lorem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque massa lorem, blandit ac nisl vitae, tempor lobortis nisi. Nulla sit amet risus eu ex vulputate tincidunt ut a est.</span>
                 </div>
             </div>
             <div class="row d-flex justify-content-center p-2">
@@ -81,13 +114,14 @@ export default styled(ProductShow)`
         background-color: #ffffff;
     }
     .synopsisBlock{
-        height : 10rem;
+        height : 12rem;
     }
     .frontTag{
         font-size: 1.5rem;
     }
     .backTag{
         font-size: 1.5rem;
+        color: #8758FF;
     }
     .manga_Tag{
         text-align : center;
@@ -97,5 +131,8 @@ export default styled(ProductShow)`
     .addBook{
         width : 9rem;
         padding: 0.5rem;
+    }
+    .synopsisText{
+        text-align : center;
     }
 `
