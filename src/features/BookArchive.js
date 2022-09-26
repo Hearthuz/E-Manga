628da112dd7929b29229252ea1597cafa8675656
SoSoStellar <githubpkaddress@gmail.com>
2022-09-26 15:49:55 +0700

    BookArchive Done

--- src/features/BookArchive.js
+++ src/features/BookArchive.js
@@ -12,32 +12,36 @@ function BookArchive({ className }) {
             <div class="bookArchive-container bg-white w-100 h-100">
                 <div class="row w-100 h-100 py-5">
                     <div class="d-flex justify-content-center alignt-item-center">
-                        <div class="d-flex w-50">
+                        <div class="d-flex py-5 w-50">
                             <input class="searchBox rounded" type="search" placeholder="Input Book Name Here" aria-label="Search"></input>
                             <button type="button" class="btn btn-outline-success mx-2">Search</button>
                         </div>
                     </div>
-                    <div class="row p-3 resultBlock">
-                        <div class="col-2" data-bs-toggle="modal" data-bs-target="#exampleModal"><img class="book_cover" src='https://i.pinimg.com/550x/28/19/d1/2819d12523097ccea2e541d972437128.jpg' /></div>
-                        <div class="col-2" data-bs-toggle="modal" data-bs-target="#exampleModal"><img class="book_cover" src='https://i.pinimg.com/550x/28/19/d1/2819d12523097ccea2e541d972437128.jpg' /></div>
-                        <div class="col-2" data-bs-toggle="modal" data-bs-target="#exampleModal"><img class="book_cover" src='https://i.pinimg.com/550x/28/19/d1/2819d12523097ccea2e541d972437128.jpg' /></div>
-                        <div class="col-2" data-bs-toggle="modal" data-bs-target="#exampleModal"><img class="book_cover" src='https://i.pinimg.com/550x/28/19/d1/2819d12523097ccea2e541d972437128.jpg' /></div>
+                    <div class="row py-3 resultBlock">
+                        <div class="col-2 py-2" data-bs-toggle="modal" data-bs-target="#selectionModal"><img class="book_cover" src='https://i.pinimg.com/550x/28/19/d1/2819d12523097ccea2e541d972437128.jpg' /></div>
+                        <div class="col-2 py-2" data-bs-toggle="modal" data-bs-target="#selectionModal"><img class="book_cover" src='https://i.pinimg.com/550x/28/19/d1/2819d12523097ccea2e541d972437128.jpg' /></div>
+                        <div class="col-2 py-2" data-bs-toggle="modal" data-bs-target="#selectionModal"><img class="book_cover" src='https://i.pinimg.com/550x/28/19/d1/2819d12523097ccea2e541d972437128.jpg' /></div>
+                        <div class="col-2 py-2" data-bs-toggle="modal" data-bs-target="#selectionModal"><img class="book_cover" src='https://i.pinimg.com/550x/28/19/d1/2819d12523097ccea2e541d972437128.jpg' /></div>
+                        <div class="col-2 py-2" data-bs-toggle="modal" data-bs-target="#selectionModal"><img class="book_cover" src='https://i.pinimg.com/550x/28/19/d1/2819d12523097ccea2e541d972437128.jpg' /></div>
+                        <div class="col-2 py-2" data-bs-toggle="modal" data-bs-target="#selectionModal"><img class="book_cover" src='https://i.pinimg.com/550x/28/19/d1/2819d12523097ccea2e541d972437128.jpg' /></div>
+                        <div class="col-2 py-2" data-bs-toggle="modal" data-bs-target="#selectionModal"><img class="book_cover" src='https://i.pinimg.com/550x/28/19/d1/2819d12523097ccea2e541d972437128.jpg' /></div>
                     </div>
                 </div>
             </div>
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal fade" id="selectionModal" tabindex="-1" aria-labelledby="selectionModalLabel" aria-hidden="true">
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <h5 class="modal-title" id="selectionModalLabel">Question</h5>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
-                            ...
+                            What do you want do?
                         </div>
                         <div class="modal-footer">
+                            <button type="button" class="btn btn-success">Edit</button>
+                            <button type="confirm" class="btn btn-danger">Delete</button>
                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Save changes</button>
                         </div>
                     </div>
                 </div>
@@ -60,6 +64,11 @@ export default styled(BookArchive)`
         width : 100%;
     }
     .book_cover{
-        width : 7rem;
+        width : 10rem;
+    }
+    @media only screen and (max-width: 992px) {
+        .book_cover{
+            width : 7rem;
+        }
     }
 `
--- src/features/NavbarAdmin.js
+++ src/features/NavbarAdmin.js
@@ -4,36 +4,36 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 function NavbarAdmin({ className }) {
-    const logo = require('../assets/logo.png');
-    return (
-        <header className={className}>
-            <div class="navbar navbar-expand-lg">
-                <div class="container-fluid p-0">
-                    <img src={logo} class="logoPic" alt="E-Manga"></img>
-                    <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <Link to="/">Add</Link>
-                            </li>
-                            <li class="nav-item">
-                                <Link to="/BookArchive">Update & Delete</Link>
-                            </li>
-                        </ul>
-                        <div class="user mb-2 mb-lg-0">
-                            <Link>Admin Page</Link>
-                        </div>
-                    </div>
-                </div>
+  const logo = require('../assets/logo.png');
+  return (
+    <header className={className}>
+      <div class="navbar navbar-expand-lg">
+        <div class="container-fluid p-0">
+          <img src={logo} class="logoPic" alt="E-Manga"></img>
+          <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <Link to="/#">Add</Link>
+              </li>
+              <li class="nav-item">
+                <Link to="/BookArchive">Update & Delete</Link>
+              </li>
+            </ul>
+            <div class="user mb-2 mb-lg-0">
+              <Link>Admin Page</Link>
             </div>
-        </header >
-    );
+          </div>
+        </div>
+      </div>
+    </header >
+  );
 }
 
 NavbarAdmin.propTypes = {
-    className: PropTypes.string.isRequired
+  className: PropTypes.string.isRequired
 };
 
 export default styled(NavbarAdmin)`
