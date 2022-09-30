93d0c893921a34fc75c16aa36cc52dda68bd3116
Min642110325 <pumin_chummeungyen.cmu.ac.th>
2022-09-30 17:23:17 +0700

    Notification_responsive

--- src/features/Notification.js
+++ src/features/Notification.js
@@ -2,11 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Navbar from './Navbar';
+import Footer from './Footer';
 
-function Home({ className }) {
+
+function Notification({ className }) {
     const slideImages1 = require('../assets/slide1.jpg');
     const slideImages2 = require('../assets/slide2.jpg');
     const slideImages3 = require('../assets/slide3.jpg');
+
+
+
+
     return (
         <div className={className}>
             <Navbar />
@@ -38,30 +44,32 @@ function Home({ className }) {
                             <span class="visually-hidden">Next</span>
                         </button>
                     </div>
-                    <div class="notification">
+                </div>
+                <div class="topManga-container">
+                    <div class="topManga">
                         <h1>Notification</h1>
-                        <div class="flex-color  rounded-4 shadow m-2">
-                            <div class="manga-box position-relative">
+                        <div class="flex-color  rounded-4 shadow m-2 ">
+                            <div class="manga-box position-relative ">
                                 <div class="container-fluid p-0 m-0">
-                                    <div class="row p-4 ">
-                                        <div class="col-3  ">
+                                    <div class="row p-0 detailFrame rounded ">
+                                        <div class="col-sm-12 col-lg-3 p-2 ">
                                             <img src="https://img.freepik.com/free-vector/smiling-girl-anime-character-poster_603843-3026.jpg?w=360" class="card-img-top" alt="..." />
                                         </div>
-                                        <div class="col-9 ">
-                                            <div class="d-flex m-4">
-                                                <div class=" newTag me-4">_________New Book!!__________</div>
+                                        <div class="col-md-9 ms-md-auto ">
+                                            <div class="d-flex m-4 col-sm-12 ">
+                                                <div class=" newTag me-4 p-0">New Book!!</div>
                                             </div>
-                                            <div class="d-flex m-4">
-                                                <div class="  fs-4 me-5">Author</div>
-                                                <div class=" backTag fs-4 ">Person</div>
+                                            <div class="d-flex m-4  p-0">
+                                                <div class="  fs-4  col-sm-3">Author</div>
+                                                <div class=" backTag fs-4 ps-2 col-sm-3 ">Person</div>
                                             </div>
-                                            <div class="d-flex m-4">
-                                                <div class="  fs-4 me-4">Publisher</div>
-                                                <div class=" backTag fs-4 ">Name</div>
+                                            <div class="d-flex m-4 p-0 ">
+                                                <div class="  fs-4  col-sm-3 ">Publisher</div>
+                                                <div class=" backTag fs-4 ps-2 col-sm-3 ">Name</div>
                                             </div>
-                                            <div class="d-flex m-4">
-                                                <div class="  fs-4 me-4">BookType</div>
-                                                <div class=" backTag fs-4 ">NameType</div>
+                                            <div class="d-flex m-4  p-0  ">
+                                                <div class="  fs-4  col-sm-3">BookType</div>
+                                                <div class=" backTag fs-4 col-sm-3 ps-2">NameType</div>
                                             </div>
 
                                         </div>
@@ -69,28 +77,28 @@ function Home({ className }) {
                                 </div>
                             </div>
                         </div>
-                        <div class="flex-color  rounded-4 shadow m-2">
-                            <div class="manga-box position-relative">
+                        <div class="flex-color  rounded-4 shadow m-2 ">
+                            <div class="manga-box position-relative ">
                                 <div class="container-fluid p-0 m-0">
-                                    <div class="row p-4 ">
-                                        <div class="col-3  ">
+                                    <div class="row p-0 detailFrame rounded ">
+                                        <div class="col-sm-12 col-lg-3 p-2 ">
                                             <img src="https://img.freepik.com/free-vector/smiling-girl-anime-character-poster_603843-3026.jpg?w=360" class="card-img-top" alt="..." />
                                         </div>
-                                        <div class="col-9 ">
-                                            <div class="d-flex m-4">
-                                                <div class=" newTag me-4">_________New Book!!__________</div>
+                                        <div class="col-md-9 ms-md-auto ">
+                                            <div class="d-flex m-4 col-sm-12 ">
+                                                <div class=" newTag me-4 p-0">New Book!!</div>
                                             </div>
-                                            <div class="d-flex m-4">
-                                                <div class="  fs-4 me-5">Author</div>
-                                                <div class=" backTag fs-4 ">Person</div>
+                                            <div class="d-flex m-4  p-0">
+                                                <div class="  fs-4  col-sm-3">Author</div>
+                                                <div class=" backTag fs-4 ps-2 col-sm-3 ">Person</div>
                                             </div>
-                                            <div class="d-flex m-4">
-                                                <div class="  fs-4 me-4">Publisher</div>
-                                                <div class=" backTag fs-4 ">Name</div>
+                                            <div class="d-flex m-4 p-0 ">
+                                                <div class="  fs-4  col-sm-3 ">Publisher</div>
+                                                <div class=" backTag fs-4 ps-2 col-sm-3 ">Name</div>
                                             </div>
-                                            <div class="d-flex m-4">
-                                                <div class="  fs-4 me-4">BookType</div>
-                                                <div class=" backTag fs-4 ">NameType</div>
+                                            <div class="d-flex m-4  p-0  ">
+                                                <div class="  fs-4  col-sm-3">BookType</div>
+                                                <div class=" backTag fs-4 col-sm-3 ps-2">NameType</div>
                                             </div>
 
                                         </div>
@@ -100,63 +108,143 @@ function Home({ className }) {
                         </div>
                     </div>
                 </div>
+
             </div>
+            <Footer />
         </div>
     )
 }
 
-Home.propTypes = {
+Notification.propTypes = {
     className: PropTypes.string.isRequired
 };
 
-export default styled(Home)`
-    .notification-container{
-        display: flex;
-        justify-content: center;
-        padding-top: 4rem;
-    }
-    .carousel-item{
-        height: 32rem;
-    }
-    .notification{
-        padding: 4rem 16rem 4rem 16rem;
-    }
-    .notification h1{
-        color: #ffffff;
-        padding-bottom: 2rem;
-    }
-    .card-notification{
-        margin:
-    }
-    .card:hover{
-        filter: blur(2px);
-    }
-    .card-button{
-        position: absolute;
-        padding: 1rem;
-        right: 0.3rem;
-    }
-    button{
-        border: none;
-        padding: 0.5rem;
-    }
-    .manga-box{
-        
-    }
-    .favorite-button{
-        margin-bottom: 0.5rem;
-        background: #ff6961;
-    }
-    .flex-color{
-        background: #d9d9d9;
-    }
-    flex-container{
+export default styled(Notification)`
+.homePage{
+    padding-top: 4rem;
+}
+.carousel-item{
+    max-height: 28rem;
+}
+.searchContainer{
+    position: absolute;
+    z-index: 1;
+    margin-top: -25px;
+}
+.searchIcon{
+    padding: 10px;
+    margin-top: -1px;
+}
+.searchBar{
+    background-color: #e5e5e5;
+    width: 75%;
+    height: 50px;
+    padding: 5px;
+}
+.searchInput{
+    border: none;
+    text-align: center;
+    border-radius: 50px 0 0 50px;   
+    background-image: linear-gradient(to right bottom, #c21500, #d74e00, #e87700, #f59e00, #ffc500);
+    color: #ffffff;
+}
+.searchInput::placeholder{
+    color: #ffffff;
+}
+.btn-search{
+    color: #ffffff;
+    background-color: #f7797d;
+    border-radius: 0 50px 50px 0;
+}
+.btn-search:hover{
+    color: #bdc3c7;
+    background-color: #f7797d;
+}
+.topManga-container{
     display: flex;
-    height: 20rem;
+    justify-content: center;
+    width: 100%;
+}
+.topManga{
+    width: 90%;
+    padding: 3rem;
+}
+.topManga h1{
+    color: #ffffff;
+    padding-bottom: 2rem;
+}
+.image_overlay{
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    display: flex;
+    background: rgb(0,0,0,0.3);
+    justify-content: center;
     align-items: center;
-    }
+    opacity: 0;
+    transition: opacity 0.25s;
+}
+.image_overlay-blur {
+    backdrop-filter: blur(5px);
+}
+.image_overlay-primary {
+    background: #009578;
+}
+.image_overlay > * {
+    transform: translateY(20px);
+    transition: transform 0.25s;
+}
+.image_overlay:hover {
+    opacity: 1;
+}
+.image_overlay:hover > * {
+    transform: translateY(0);
+}
+.card-button{
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: space-center;    
+}
+button{
+    border: none;
+}
+.mangaName{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1rem;
+    font-weight: bold;
+    color: #ffffff;
+    text-align: center;
+}
+.manga-box{
+    padding-bottom: 1rem;
+}
+.cart-button{
+    margin: 0.5rem;
+    padding: 0.5rem;
+    width: 100%;
+}
+.cart{
+    display: flex;
+    justify-content: flex-end;
+    align-items: flex-end
+}
+.favorite{
+    display: flex;
+    justify-content: flex-end;
+}
+.favorite-button{
+    margin: 0.5rem 0.5rem 0;
+    padding: 0.5rem;
+    height: 50%;
+    width: auto;
+    background: #ff6961;    
+}
     .detailFrame{
         background-color: #d9d9d9;
+        
     }
     .newTag{
     font-size: 40px;
